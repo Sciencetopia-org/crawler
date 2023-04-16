a939c718f8a710661279a95e716c0a3477683358
Ahasier <82276410+83035706yf@users.noreply.github.com>
2023-04-16 15:13:32 -0500

    third commit

---
+++ Feynman_scraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+base_url = "https://www.feynmanlectures.caltech.edu"
+volume_links = ["/I_toc.html", "/II_toc.html", "/III_toc.html"]
+
+with open("feynman_lectures.csv", "w", newline='', encoding='utf-8') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Volume", "Chapter Number", "Title", "URL"])
+
+    for volume_link in volume_links:
+        url = base_url + volume_link
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        chapters = soup.find_all("div", class_="toc-chapter")
+
+        for chapter in chapters:
+            chapter_number = chapter.find("span", class_="tag").text.strip()
+            chapter_title = chapter.find("a", class_="chapterlink").text.strip()
+            chapter_link = chapter.find("a", class_="chapterlink")["href"]
+            chapter_url = base_url + "/" + volume_link.split("_")[0][1:] + "/" + chapter_link
+
+            csv_writer.writerow([volume_link.split("_")[0][1:], chapter_number, chapter_title, chapter_url])
+
+print("Done!")
